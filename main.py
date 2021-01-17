1ee9b0f761a345c8ad95329ecc5c740d1db0c997
mmt <pycdremail@gmail.com>
2021-01-17 14:28:31 +0330

    add logs to main file

--- main.py
+++ main.py
@@ -1,32 +1,43 @@
-from generator import convert
-from sound import Audio, getaudio
-
 def main():
+	from log import log
+	log.info("start main proccess and importing")
+	from generator import convert
+	from sound import Audio, getaudio
 	from argparse import ArgumentParser
 	from os import system, listdir, makedirs, get_terminal_size as size
 	from os.path import exists, join
-
+	log.info("imported")
+	
+	log.info("parsing arguments...")
 	parser = ArgumentParser()
 	parser.add_argument("path")
 	parser.add_argument("-o", "--out", required = False)
 	parser.add_argument("--width", type = int, required = False)
 	parser.add_argument("--height", type = int, required = False)
 	args = parser.parse_args()
+	log.info("parsed")
 	
 	path = args.path
 	out = args.out or "output"
 	if not exists(out):
 		makedirs(out)
+	else:
+		log.warning("output path is existing. text files will be overwritten.")
+	log.info("set width and height of output...")
 	width = args.width or size().columns
 	height = args.height or size().lines
 	
+	log.info("start convert proccess")
 	details = convert(path, out, width, height)
 	fps = details["fps"]
 	
+	log.info("extracting audio...")
 	getaudio(path, join(out, "sound.mp3"))
+	log.info("extracted. make Audio object")
 	audio = Audio(join(out, "sound.mp3"))
-	audio.start()
+	log.info("start sound player and texts reader proccess...")
 	
+	audio.start()
 	system(f"go run reader.go {fps} {out}")
 
 if __name__ == "__main__":
